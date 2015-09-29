44d987de07439e2a48820d3fab51b99eafcef225
patrick Yang <b98901136@ntu.edu.tw>
2015-09-29 12:23:33 +0900

    for test

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params()[:article])
+    if @article.save 
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+  def article_params
+    params.require(:article).permit(:title, :text, :comment)
+  end
+end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
     @action_name = action_name
-    
+        
   end
 end
